51cea83a57c1ce1286fdce46a891c4bd4b36f3c7
devgoel2004 <devgoel12072004@gmail.com>
2026-03-26 23:10:02 +0530

    Added persistence using InMemorySaver

--- main.py
+++ main.py
@@ -2,11 +2,14 @@ from langgraph.graph import StateGraph, START, END
 from typing import TypedDict, Annotated
 from langchain_core.messages import BaseMessage, HumanMessage
 from langchain_google_genai import ChatGoogleGenerativeAI
+from langgraph.checkpoint.memory import InMemorySaver
 import os
 from dotenv import load_dotenv
 from langgraph.graph.message import add_messages
 load_dotenv()
 google_api_key = os.getenv("GOOGLE_API_KEY")
+
+checkpointer = InMemorySaver()
 
 class ChatState(TypedDict):
     messages: Annotated[list[BaseMessage], add_messages]
@@ -28,11 +31,17 @@ graph.add_node('chat_node', chat_node)
 graph.add_edge(START, 'chat_node')
 graph.add_edge('chat_node', END)
 
-chatbot = graph.compile()
+config = {
+    "configurable":{
+        "thread_id":"user_id"
+    }
+}
+
+chatbot = graph.compile(checkpointer=checkpointer)
 
 initial_state = {
     'messages':[HumanMessage(content="What is the capital of India")]
 }
 
-response = chatbot.invoke(initial_state)
+response = chatbot.invoke(initial_state, config = config)
 print(response)
